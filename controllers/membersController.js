bc3a6cef78a7e37af63be2f97615768191c1ef68
Dhiva Ryan H <176643369+d-hardine@users.noreply.github.com>
2025-08-21 20:55:09 +0700

    sign up validation added

--- controllers/membersController.js
+++ controllers/membersController.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcryptjs')
 const passport = require('passport')
 const db = require('../db/queries')
 const fns = require('date-fns')
+const { body, validationResult } = require('express-validator')
 
 const indexGet = async (req, res) => {
     const allMessages = await db.getAllMessages()
@@ -23,17 +24,34 @@ const registerGet = (req, res) => {
         res.render('register')
 }
 
-const registerPost = async (req, res, next) => {
+const validateUser = [
+    body('firstname').trim()
+        .isAlpha().withMessage(`First name must only contain letters.`)
+        .isLength({min: 3, max: 15}).withMessage(`First name must be between 3 and 15 characters.`),
+    body('lastname').trim()
+        .isAlpha().withMessage(`Last name must only contain letters.`)
+        .isLength({min: 3, max: 15}).withMessage(`Last name must be between 3 and 15 characters.`),
+    body('username').trim()
+        .isLength({min: 3, max: 15}).withMessage(`Username must be between 3 and 15 characters.`),
+    body('password').trim()
+        .isLength({min: 8}).withMessage(`Password must be at least 8 characters.`)
+]
+
+const registerPost = [validateUser, async (req, res, next) => {
     try {
-        const hashedPassword = await bcrypt.hash(req.body.password, 10)
-        await db.registerNewUser(req.body.firstname, req.body.lastname, req.body.username, hashedPassword, false)
-        //res.redirect('/')
-        next()
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(400).render("register", {errors: errors.array()})
+        } else {
+            const hashedPassword = await bcrypt.hash(req.body.password, 10)
+            await db.registerNewUser(req.body.firstname, req.body.lastname, req.body.username, hashedPassword, false)
+            next()
+        }
     } catch(err) {
         console.error(err)
         return next(err)
     }
-}
+}]
 
 const loginGet = (req, res) => res.render('login')
 
